a4b679e4e4da874ae4b7a01e01b766e6a6250f2e
Sydney Sisco <hello@sydsis.co>
2023-06-05 16:25:13 -0700

    jot your thoughts are saved to indexeddb

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,56 +1,60 @@
 import { useState, useEffect } from 'react'
-import { ApiTest } from './components/ApiTest'
 import { socket } from './utils/socket'
-import { SocketTest } from './components/SocketTest'
-import { ConnectionState } from './components/ConnectionState'
 import futureLogo from '/future.svg'
 import './App.css'
+import Dexie from 'dexie';
+
+const db = new Dexie("ThoughtsDB");
+db.version(1).stores({ thoughts: "++id,text" });
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [thoughts, setThoughts] = useState([]);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
+    const loadThoughts = async () => {
+      const data = await db.thoughts.toArray();
+      setThoughts(data);
     };
+    loadThoughts();
   }, []);
+
+  const addThought = async (event) => {
+    event.preventDefault();
+    if (!input.trim()) return;
+    await db.thoughts.add({ text: input });
+    setInput("");
+    const updatedThoughts = await db.thoughts.toArray();
+    setThoughts(updatedThoughts);
+  };
 
   return (
     <>
       <div>
-        <a href="https://github.com/sydney-sisco/webapp-template" target="_blank">
+        <a href="https://github.com/sydney-sisco/jot-your-thought" target="_blank">
           <img src={futureLogo} className="logo" alt="future logo" />
         </a>
       </div>
-      <h1>webapp-template</h1>
-      <p className="read-the-docs">
-        Click on the logo to learn more
-      </p>
+      <h1>Jot Your Thought</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <form onSubmit={addThought}>
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            rows="4"
+            placeholder="Write your thought here..."
+          ></textarea>
+          <button type="submit">Save</button>
+        </form>
       </div>
-      <ApiTest />
-      <SocketTest />
-      <ConnectionState isConnected={ isConnected } />
+      <div className="card">
+        <ul>
+          {thoughts.map((thought) => (
+            <li key={thought.id}>{thought.text}</li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
