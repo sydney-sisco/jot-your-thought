89b9d9731aa1d354c1d90ec2f55cd7f26fed1960
Sydney Sisco <hello@sydsis.co>
2023-07-09 18:18:05 -0700

    put it all together

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import { useState, useEffect } from 'react'
-import { socket } from './utils/socket'
+import { getSocket } from './utils/socket'
 import futureLogo from '/future.svg'
 import './App.css'
 import Dexie from 'dexie';
 import { usePersistence } from './hooks/usePersistence';
 import Login from './components/Login';
 import Register from './components/Register';
+import Logout from './components/Logout';
+
 import { Jot } from './components/Jot';
 import { Thoughts } from './components/Thoughts';
 
 import { Link, Route } from "wouter";
+import { SocketTest } from './components/SocketTest';
+
+import { useLocalStorage } from './hooks/useLocalStorage';
+import { useReadLocalStorage } from './hooks/useReadLocalStorage';
 
 const db = new Dexie("ThoughtsDB");
 db.version(1).stores({ thoughts: "++id,text" });
@@ -18,6 +24,7 @@ function App() {
 
   const { deviceId } = usePersistence();
   const [thoughts, setThoughts] = useState([]);
+  const [token, setToken] = useLocalStorage("token", null);
 
   useEffect(() => {
     const loadThoughts = async () => {
@@ -40,7 +47,7 @@ function App() {
     
     await db.thoughts.add(thought);
 
-    socket.emit("new thought", thought);
+    getSocket().emit("new thought", thought);
 
     const updatedThoughts = await db.thoughts.toArray();
     setThoughts(updatedThoughts);
@@ -52,14 +59,22 @@ function App() {
         <a><img src={futureLogo} className="logo" alt="future logo" /></a>
       </Link>
       <h1>Jot Your Thought</h1>
-      <Link href="/login">
-        <a className="link">Login</a>
-      </Link>
-      <Link href="/register">
-        <a className="link">Register</a>
-      </Link>
+      {token ?
+      <>
+        <Logout setToken={setToken} />
+      </>
+      :
+      <>
+        <Link href="/login">
+          <a className="link">Login</a>
+        </Link>
+        <Link href="/register">
+          <a className="link">Register</a>
+        </Link>
+      </>
+      }
       <Route path="/login">
-        <Login />
+        <Login setToken={setToken} />
       </Route>
       <Route path="/register">
         <Register />
@@ -68,6 +83,7 @@ function App() {
         <Jot addThought={addThought} />
         <Thoughts thoughts={thoughts} />
       </Route>
+      <SocketTest />
     </>
   )
 }
