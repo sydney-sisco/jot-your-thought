b8c78f6e35e994d6df29e4c5172d2d7be7584d10
Sydney Sisco <hello@sydsis.co>
2023-07-09 18:13:46 -0700

    update socket.js to authenticate with a jwt

--- backend/utils/socket.js
+++ backend/utils/socket.js
@@ -1,9 +1,18 @@
 const { Server } = require("socket.io");
+const jwt = require('jsonwebtoken');
 
 module.exports = function (server) {
   const io = new Server(server);
 
-  io.on('connection', (socket) => {
+  io.use((socket, next) => {
+    console.log('socket.handshake.query', socket.handshake.query);
+    const token = socket.handshake.query.token;
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) return next(new Error('Authentication error'));
+      socket.decoded = decoded;
+      next();
+    });
+  }).on('connection', (socket) => {
     console.log('a user connected');
   
     socket.on('disconnect', () => {
