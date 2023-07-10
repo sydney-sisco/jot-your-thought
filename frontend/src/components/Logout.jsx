faf087f13e8297fc20308b940a583d19b14451f7
Sydney Sisco <hello@sydsis.co>
2023-07-09 18:10:33 -0700

    add a logout component

---
+++ frontend/src/components/Logout.jsx
@@ -0,0 +1,28 @@
+import { useLocation } from "wouter";
+import { disconnectSocket } from "../utils/socket";
+
+function Logout({ setToken }) {
+
+  const [location, setLocation] = useLocation();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // remove token from local storage
+    setToken(null);
+
+    // disconnect socket
+    disconnectSocket();
+
+    // redirect to login page
+    setLocation("/login");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Logout</button>
+    </form>
+  );
+}
+
+export default Logout;
