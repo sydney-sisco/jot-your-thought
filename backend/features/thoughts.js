b9d3fe8959836a1dedcbac11ea0abeb403e4eb40
Sydney Sisco <hello@sydsis.co>
2023-07-21 17:12:37 -0400

    add functionality to edit and delete thoughts. Change firestore structure to store thoughts nested under user

--- backend/features/thoughts.js
+++ backend/features/thoughts.js
@@ -1,5 +1,6 @@
 const firestore = require('../utils/firestore')
 
+// adds a thought to a device's collection (old)
 const addThought = async (thoughtData) => {
   try {
     const { id, text, deviceId, timestamp } = thoughtData;
@@ -16,6 +17,59 @@ const addThought = async (thoughtData) => {
     console.log('Thought added successfully');
   } catch (error) {
     console.error('Error adding thought:', error);
+  }
+};
+
+// saves a thought to a user's collection
+const saveThought = async (thoughtData, userId) => {
+  try {
+    const { id, text, deviceId, timestamp } = thoughtData;
+    const thoughtRef = firestore
+      .collection('users')
+      .doc(userId)
+      .collection('thoughts')
+      .doc(id);
+    await thoughtRef.set({
+      text,
+      deviceId,
+      timestamp,
+    });
+    console.log('Thought added successfully');
+  } catch (error) {
+    console.error('Error adding thought:', error);
+  }
+};
+
+// delete a thought
+const deleteThought = async (thoughtId, userId) => {
+  try {
+    const thoughtRef = firestore
+      .collection('users')
+      .doc(userId)
+      .collection('thoughts')
+      .doc(thoughtId);
+    await thoughtRef.delete();
+    console.log('Thought deleted successfully');
+  } catch (error) {
+    console.error('Error deleting thought:', error);
+  }
+};
+
+// edit a thought
+const editThought = async (thoughtData, userId) => {
+  try {
+    const { id, text } = thoughtData;
+    const thoughtRef = firestore
+      .collection('users')
+      .doc(userId)
+      .collection('thoughts')
+      .doc(id);
+    await thoughtRef.update({
+      text,
+    });
+    console.log('Thought edited successfully');
+  } catch (error) {
+    console.error('Error editing thought:', error);
   }
 };
 
@@ -43,7 +97,19 @@ module.exports = (io) => {
   io.on('connection', (socket) => {
     socket.on('new thought', (thought) => {
       console.log('new thought', thought);
-      addThought(thought);
+      // addThought(thought);
+      saveThought(thought, socket.decoded.username);
     });
+
+    socket.on('delete thought', (thoughtId) => {
+      console.log('delete thought', thoughtId);
+      deleteThought(thoughtId, socket.decoded.username);
+    });
+
+    socket.on('edit thought', (thought) => {
+      console.log('edit thought', thought);
+      editThought(thought, socket.decoded.username);
+    });
+
   });
 }
